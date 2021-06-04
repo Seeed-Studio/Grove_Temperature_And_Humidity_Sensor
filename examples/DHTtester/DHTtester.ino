c49c1b76d08a9e5c81027508781d4e786ef934bf
IsQianGe <52726080+IsQianGe@users.noreply.github.com>
2021-06-04 10:49:15 +0800

    Update DHTtester.ino

--- examples/DHTtester/DHTtester.ino
+++ examples/DHTtester/DHTtester.ino
@@ -3,26 +3,25 @@
 
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to（DHT10 and DHT20 don't need define it）
-
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
+//#define DHTTYPE DHT10   // DHT 10
+//#define DHTTYPE DHT20   // DHT 20
 
 /*Notice: The DHT10 and DHT20 is different from other DHT* sensor ,it uses i2c interface rather than one wire*/
 /*So it doesn't require a pin.*/
-
-//#define DHTTYPE DHT10
-//#define DHTTYPE DHT20
+#define DHTPIN 2     // what pin we're connected to（DHT10 and DHT20 don't need define it）
+DHT dht(DHTPIN, DHTTYPE);   //   DHT11 DHT21 DHT22
+//DHT dht(DHTTYPE);         //   DHT10 DHT20 don't need to define Pin
 
 // Connect pin 1 (on the left) of the sensor to +5V
 // Connect pin 2 of the sensor to whatever your DHTPIN is
 // Connect pin 4 (on the right) of the sensor to GROUND
 // Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
 
-DHT dht(DHTPIN, DHTTYPE);   //DHT11 DHT21 DHT22
-//DHT dht(DHTTYPE);    //DHT10 DHT20 don't need to define Pin
+
 #if defined(ARDUINO_ARCH_AVR)
     #define debug  Serial
 
